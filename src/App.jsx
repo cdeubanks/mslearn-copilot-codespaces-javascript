d09ae73f502b9b0907496fc633ae5b15670062a9
cdeubanks <35720519+cdeubanks@users.noreply.github.com>
2026-07-25 23:59:46 +0000

    Update siteProps with new user information and enhance social icon styles

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Hector Salamanca",
+  title: "Vibe Ambassador",
+  email: "hector@salamanca.com",
+  gitHub: "donhector",
+  instagram: "donhector",
+  linkedIn: "hectorsalamanca",
+  medium: "donhector",
+  twitter: "donhector",
+  youTube: "donhector",
 };
 
 const primaryColor = "#4E567E";
